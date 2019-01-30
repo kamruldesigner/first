c535e2afa5ce924f4d6f34b9541b669bfb64497a
Your Name <you@example.com>
2019-01-30 13:34:33 +0600

    first commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Container } from 'semantic-ui-react'
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
+import NavBar from "../../features/nav/NavBar/NavBar.jsx"
+
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Container className = "main">
+          <EventDashboard />
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
